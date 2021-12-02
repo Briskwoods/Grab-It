b570c97da6d0aadcf522ccdabc28dae5dfc38055
Jeff Gichuki <julian.gichuki@gmail.com>
2021-12-02 18:09:30 +0300

    Update KeyboardManager.cs
    Added Restart, RereshEverything, ClearFields, DelayBeforeRestart and changed Validation to validate full words instead of validating per letter and added a backspace functionality.

--- KeyboardManager.cs
+++ KeyboardManager.cs
@@ -8,6 +8,7 @@ public class KeyboardManager : MonoBehaviour
 {
     public char[] dictionary;                        // This is the word/item we're trying to identify split into letters, for now is initialised here but can be obtained from the game manager and split into this list on a per letter basis
 
+    public char[] m_typedLetter;
     public char[] m_tempHold;
 
     [SerializeField] private GameManager m_gameManager;
@@ -16,44 +17,122 @@ public class KeyboardManager : MonoBehaviour
 
     public int counter = 0;
 
+    public string m_dictionaryWord;
+    public string m_guessedWord;
+
+    public float m_restartDelay = 2.5f;
+
+    public WrongAnswerManager WrongAnswerManager_;
+
+    public CorrectAnswerManager CorrectAnswerManager_;
+
+    public ExtendArm ExtendArm_;
+
+    public FailHandSequence FailHandSequence_;
+
+    public GrabSequence GrabSequence_;
+
+    public SlotsAdjust UpdateSlotsAdjust;
+
     // Start is called before the first frame update
     void Start()
     {
-        // For the first word, will add a function to split on completion of a word/level
-        //dictionary = m_gameManager.m_wordToType.Split(char.Parse(" "));
-
-        dictionary = m_gameManager.m_wordToType.ToLower().ToCharArray(); //Evans - Always convert to lower case before converting to char
-
-        for (int i = 0; i < dictionary.Length; i++)
-        {
-            Debug.Log(dictionary[i]);
-        }
+        RefreshEverything();
     }
 
     public void KeyPressed(string _key)
     {
+        //Debug.Log(_key);
+
         ValidationCheck(_key);
+    }
+    
+
+    public void BackSpace()
+    {
+        if (counter > 0) //Check if we are at the start of the word
+        {
+            counter--;
+            inputFields[counter].text = "";
+            //Grow Arm
+            ExtendArm_.ReduceSize();
+        }
     }
 
     public void ValidationCheck(string letter)
     {
-        // Clears only where letter is wrong
-        m_tempHold = letter.ToLower().ToCharArray();//Evans - Convert To Lower then to Char
-        switch (m_tempHold[0].Equals(dictionary[counter]) /* Not actual check condition */)
+        m_typedLetter = letter.ToLower().ToCharArray();
+        m_tempHold[counter] = m_typedLetter[0];
+        inputFields[counter].text = "" + letter;
+        counter++;
+
+        //Grow Arm
+        ExtendArm_.IncreaseSize();
+
+        switch (counter.Equals(dictionary.Length)) //Check if the word is complete before validation
         {
             case true:
-                Debug.Log("True");
-                inputFields[counter].text = "" + letter;
-                
-                counter++;
-                // Code to Go to Next Line 
-                Debug.Log("Next Line");
+                // Collate both dictionaries and check if words are the same
+                m_dictionaryWord = new string(dictionary);
+                m_guessedWord = new string(m_tempHold);
 
-
+                switch (m_guessedWord.Equals(m_dictionaryWord))
+                {
+                    case true:
+                        // On level complete
+                        GrabSequence_.GrabObject();
+                        CorrectAnswerManager_.StartPump();
+                        break;
+                    case false:
+                        WrongAnswerManager_.StartPump();
+                        FailHandSequence_.StartFailSequence();
+                        StartCoroutine(DelayBeforeRestart(m_restartDelay));
+                        break;
+                }
                 break;
             case false:
-                Debug.Log("False");
                 break;
         }
+    }
+
+    public void Restart()
+    {
+        counter = 0;
+        m_tempHold = new char[dictionary.Length];
+        ClearFields(inputFields);
+    }
+
+    public void ClearFields(TextMeshProUGUI[] inputFields)
+    {
+        for (int i = 0; i < inputFields.Length; i++)
+        {
+            switch (inputFields[i].enabled == true)
+            {
+                case true:
+                    inputFields[i].text = "";
+                    break;
+                case false:
+                    break;
+            }
+        }
+    }
+
+    IEnumerator DelayBeforeRestart(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Restart();
+    }
+
+
+    // Used on level reset
+    [ContextMenu("Refresh Everything")]
+    public void RefreshEverything()                 
+    {
+        counter = 0;
+        dictionary = null;
+        dictionary = ControlCenter.Instance.GameManager_.m_wordToType.ToLower().ToCharArray();
+        m_tempHold = new char[dictionary.Length];
+        UpdateSlotsAdjust.AdjustWordLength(dictionary.Length);
+        ClearFields(inputFields);
     }
 }
